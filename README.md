3d58d3eb19cb22e94243e44802a95797e9bb9c42
moseshi <moseshi@gmail.com>
2020-08-03 01:09:02 +0900

    add description and constants.sample.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# speaks
+# 使用の準備
+constants.sample.pyをコピーし、各値を適切に書き換える。
+```console
+cp constants.sample.py constants.py
+# edit constants.py
+```
+依存ライブラリをインストールし、起動。
+```console
+pip install -r requirements.txt
+python Mintbot.py
+```
---
+++ constants.sample.py
@@ -0,0 +1,12 @@
+# botを使用するサーバのID
+SERVER_ID = 1234
+# ユーザがサーバに参加した際に紹介する必要のあるチャンネルのID
+README_CH_ID = 1234
+CHANNEL_GUIDE_CH_ID = 1234
+SELFINTRO_CH_ID = 1234
+GREETING_CH_ID = 1234
+JOIN_LOG_CH_ID = 1234
+# テキストチャンネルをボイスチャットへ読み上げ機能に使用する情報
+# 読み上げるテキストチャンネルのIDとその読み上げ先のボイスチャンネルのID
+VC_GENERAL_CH_ID = 1234
+VC_TEXT_GENERAL_CH_ID = 1234
